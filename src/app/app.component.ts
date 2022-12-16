d355c4dbae694e01f7216960a459dfdd269515a0
Erick Ortiz <92346372+erickortizc@users.noreply.github.com>
2022-12-16 11:46:04 -0600

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto001';
+  nombre ='Erick Ortiz';
+  edad = 9;
+  email = 'ericks.23@gmail.com';
+  sueldos =[1700,1600,1900];
+  activo = true;
+
+esActivo(){
+  if(this.activo){
+    return 'Trabajador activo';
+  }else{
+    return 'Trabajador inactivo';
+  }
+}
+ultimos3Sueldos(){
+  let suma=0;
+  for (let x = 0; x < this.sueldos.length; x++) {
+    suma+= this.sueldos[x];
+  }
+  return suma;
+}
 }
