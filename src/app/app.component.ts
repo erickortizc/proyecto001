356f5e45d3e08e741032b1e74a787eafc81135dc
erick.ortiz <erick.ortiz.cursoinfotec@gmail.com>
2022-12-30 11:26:40 -0600

    uno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre ='Erick Ortiz';
-  edad = 9;
-  email = 'ericks.23@gmail.com';
-  sueldos =[1700,1600,1900];
-  activo = true;
+  contador =1;
+  nombre = "";
+  apellido ="";
+  incrementar(){
+    this.contador++;
+  }
+  decrementar(){
+    this.contador--;
+  }
 
-esActivo(){
-  if(this.activo){
-    return 'Trabajador activo';
-  }else{
-    return 'Trabajador inactivo';
-  }
-}
-ultimos3Sueldos(){
-  let suma=0;
-  for (let x = 0; x < this.sueldos.length; x++) {
-    suma+= this.sueldos[x];
-  }
-  return suma;
-}
 }
